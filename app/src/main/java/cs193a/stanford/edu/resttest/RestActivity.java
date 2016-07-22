31080af357b30e0dbf17da1b3ce15ac6bb1b0de4
Santi Gallego <santigallego9@gmail.com>
2016-07-21 19:24:12 -0500

    added chuck norris and cat pics api functionality

--- app/src/main/java/cs193a/stanford/edu/resttest/RestActivity.java
+++ app/src/main/java/cs193a/stanford/edu/resttest/RestActivity.java
@@ -8,18 +8,105 @@
 
 package cs193a.stanford.edu.resttest;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.*;
 import android.widget.*;
+
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.async.http.body.JSONArrayBody;
+import com.koushikdutta.ion.Ion;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
 import stanford.androidlib.*;
+import stanford.androidlib.xml.XML;
 
 public class RestActivity extends SimpleActivity {
+
+    private static final String SUCCESS = "success";
+    private int width, height;
+
     // auto-generated
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rest);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        width = size.x;
+        height = size.y;
     }
+
+
+    public void chuckNorrisClick(View view) {
+        Ion.with(this)
+                .load("http://api.icndb.com/jokes/random")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        processChuckNorrisData(result);
+                    }
+                });
+    }
+
+
+    public void catClick(View view) {
+        Ion.with(this)
+                .load("http://thecatapi.com/api/images/get?results_per_page=4&format=xml")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        processCatData(result);
+                    }
+                });
+    }
+
+    /*
+     *
+     * The JSON data uses the following format:
+     *
+     *  {
+     *   "type": "success",
+     *   "value": {
+     *              "id": 496,
+     *              "joke": "Chuck Norris went out of an infinite loop.",
+     *              "categories": ["nerdy"]
+     *            }
+     *  }
+     */
+
+    private void processChuckNorrisData(String data) {
+        try {
+            JSONObject json = new JSONObject(data);
+            if(json.getString("type").equals(SUCCESS)) {
+                JSONObject value = json.getJSONObject("value");
+                String joke = value.getString("joke");
+                JSONArray categories = value.getJSONArray("categories");
+
+                TextView output = (TextView) findViewById(R.id.output);
+                output.setText(joke);
+            } else {
+                Log.e("JSON", "Json failed");
+            }
+        } catch (JSONException e) {
+            Log.wtf("json", e);
+        }
+    }
+
+
 
 
     /*
@@ -36,18 +123,44 @@ public class RestActivity extends SimpleActivity {
      * }
      */
 
-    /*
-     *
-     * The JSON data uses the following format:
-     *
-     *  {
-     *   "type": "success",
-     *   "value": {
-     *              "id": 496,
-     *              "joke": "Chuck Norris went out of an infinite loop.",
-     *              "categories": ["nerdy"]
-     *            }
-     *  }
-     */
+    private void processCatData(String data) {
+        try {
+            JSONObject json = XML.toJSONObject(data);
+            JSONArray jsonArray = json
+                    .getJSONObject("response")
+                    .getJSONObject("data")
+                    .getJSONObject("images")
+                    .getJSONArray("image");
+
+            GridLayout gridLayout = (GridLayout) findViewById(R.id.grid);
+            gridLayout.removeAllViews();
+
+            for(int i = 0; i < jsonArray.length(); i++) {
+                String url = jsonArray.getJSONObject(i).getString("url");
+                Log.v("URL", url);
+
+                ImageView imageView = new ImageView(this);
+                ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+                imageView.setLayoutParams(params);
+
+                int padding = (int) (getResources().getDimension(R.dimen.activity_horizontal_margin) / getResources().getDisplayMetrics().density);
+
+                Log.d("DIMENS", "" + padding);
+
+                int picSize = (width - padding * 2) / 2 - 20;
+
+                Picasso.with(this)
+                        .load(url)
+                        .resize(picSize, picSize)
+                        .into(imageView);
+                gridLayout.addView(imageView);
+            }
+        } catch(JSONException e) {
+            Log.wtf("JSON", "Json failed");
+        }
+    }
 
 }
